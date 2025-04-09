e38d9083414e418e6eb8e2a95445ff4859e45f71
onazemaq90 <onazemaq90@hotmail.com>
2025-04-09 11:12:38 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -115,8 +115,9 @@ async function Raise(json_error, status_code) {
 // ---------- UUID Generator ---------- //
 
 async function UUID() {
-    return 'xxxxxxxx-xxxx-Firex-Sx-bot-xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
